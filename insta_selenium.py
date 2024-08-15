be63c110d2e102ce4777d56b3675c65436b8e7fa
Riffat-Khan <rifaly1100@gmail.com>
2024-08-15 23:37:48 +0500

    feed_scraper

--- insta_selenium.py
+++ insta_selenium.py
@@ -1,9 +1,7 @@
 import time
 import os
+import pyperclip
 from dotenv import load_dotenv
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
@@ -22,70 +20,91 @@ url = 'https://www.instagram.com/'
 service = ChromeService(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
 driver.get(url)
+time.sleep(30)
 
 try:
     username_input = driver.find_element(By.NAME, 'username')
     username = os.getenv('INSTAGRAM_USERNAME')
-    username_input.send_keys(username)
+    username_input.send_keys('sajara6355')
     
     password_input = driver.find_element(By.NAME, 'password')
     password = os.getenv('INSTAGRAM_PASSWORD')
-    password_input.send_keys(password)
+    password_input.send_keys('Riffat@1100')
 
     login_button = driver.find_element(By.CSS_SELECTOR, 'button._acan._acap._acas._aj1-._ap30')
     login_button.click()
     
-    time.sleep(20) 
+    time.sleep(30) 
     
     save_button = WebDriverWait(driver, 5).until(
     EC.element_to_be_clickable((By.XPATH, '//div[@role="button" and contains(text(), "Not now")]'))
     )
     save_button.click()
     
-    not_now_button = WebDriverWait(driver, 5).until(
+    time.sleep(5) 
+    
+    not_now_button = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.CSS_SELECTOR, 'button._a9--._ap36._a9_1'))
     )
     not_now_button.click()
+    time.sleep(10) 
     
     articles = driver.find_elements(By.TAG_NAME, 'article')
     
     for i, article in enumerate(articles[:1]):
-        print(article.text)
-        
         post_image_element = article.find_elements(By.TAG_NAME, 'img')
         if len(post_image_element) > 1:
             image_element = post_image_element[1]
             image_url = image_element.get_attribute('src')
             print("Image URL:", image_url)
         else:
-            video_element = driver.find_element(By.TAG_NAME, 'video')
-            video_url = video_element.get_attribute('src')
-            print(f"Video URL: {video_url}")
+            time.sleep(10)
+            svg_element = article.find_element(By.CSS_SELECTOR ,'svg.x1lliihq.x1n2onr6.x5n08af')
+            svg_element.click()
             
+            time.sleep(10)
+            copy_link_button = WebDriverWait(article, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[normalize-space(text())='Copy link']"))
+    )
+            time.sleep(5)
+            if copy_link_button:
+                copy_link_button.click()
+                
+                time.sleep(5)
+                copied_link = pyperclip.paste()
+                print("Copied link:", copied_link)
             
+        wait = WebDriverWait(article, 10)
+        caption_element = wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'span._ap3a._aaco._aacu._aacx._aad7._aade')) 
+        )
+        
+        time.sleep(10)    
+        caption = caption_element.text
+        print("Caption:", caption)
+            
+        time.sleep(10)
         if "Liked by" in article.text:
             parts = article.text.split('Liked by')
             if len(parts) > 1:
                 likes_part = parts[1].split('\n')[0].strip()
                 print("Likes:", likes_part)
+                
             
-            
-        captions = driver.find_elements(By.CSS_SELECTOR, 'article div span.x1lliihq')
-        for caption in captions:
-            print('Caption: ', caption.text)
-                   
-        
         service = ChromeService(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service, options=options)
 
-        username_element = driver.find_element(By.CSS_SELECTOR, 'span._ap3a')
+        username_element = article.find_element(By.CSS_SELECTOR, 'span._ap3a')
         username = username_element.text 
         profile = f'{url}{username}/'
         driver.get(profile)
         
         try:
+            wait = WebDriverWait(driver, 10)
+            wait.until(EC.presence_of_element_located((By.XPATH, '//ul/li')))
+
             list_items = driver.find_elements(By.XPATH, '//ul/li')
-            print(f'Followers of {username}: ', list_items[1].text)
+            print(f'Followers of {username}: ',list_items[1].text)
 
         except Exception as e:
             print("Error:", e)
@@ -98,3 +117,4 @@ except Exception as e:
               
 finally:
     driver.quit()
+    
